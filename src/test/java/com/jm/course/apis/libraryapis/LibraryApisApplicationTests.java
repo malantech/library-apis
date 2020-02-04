ddc29b36c7d23b76be74e8c5a5b76be9873014c1
Malan Technologies <jmalan@malantech.com>
2020-02-04 16:08:47 -0700

    Initial Commit

---
+++ src/test/java/com/jm/course/apis/libraryapis/LibraryApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm.course.apis.libraryapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
